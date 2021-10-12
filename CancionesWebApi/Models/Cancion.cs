1a00e42ad4fe3d3ace1c95d29c5d366f7faddd8d
Kamila-Arzadum-Jimenez <a2020111543@estudiantes.upsa.edo.bo>
2021-10-12 00:03:39 -0400

    Agregar archivos de proyecto.

---
+++ CancionesWebApi/Models/Cancion.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CancionesWebApi.Models
+{
+    public class Cancion
+    {
+        [Key]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Autor { get; set; }
+       
+        public string Letra { get; set; }
+       
+        public string Enlace { get; set; }
+    }
+}
